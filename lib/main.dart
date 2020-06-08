5bc64d9df16eca9c0a11a97a973896f9ffdd2073
xuehan <xuehan868@gmail.com>
2020-06-08 14:27:14 -0700

    add new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  //instead of build all the things in the app,
+  //it just call this build method, click hot reload will be very quick
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile.JPG'),
+              ),
+              Text('Sherry Cheng',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                'Students of Computer Software Engineering',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 15,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
